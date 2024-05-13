5c29ecf63c660805f82bedb42a46f97e9b4dcc7c
ym5754n <29919765+ym5754n@users.noreply.github.com>
2024-05-13 23:25:21 +0900

    書籍の画像とタイトルを表示

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@
 import { useSearch } from "@/hooks/useSearch";
 import { useEffect } from "react";
 
+import Image from "next/image";
+
 export default function Home() {
   const [result, search] = useSearch("テスト");
 
@@ -11,8 +13,26 @@ export default function Home() {
   }, []);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {result?.items?.map((data) => <div key={data.id}>{data.id}</div>)}
+    <main className="flex min-h-screen flex-col justify-between p-5">
+      <ul className="">
+        {result?.items?.map((data) => (
+          <li key={data.id} className="p-2">
+            <div className="flex w-full">
+              <div>
+                <Image
+                  src={data.volumeInfo.imageLinks.smallThumbnail}
+                  alt={data.volumeInfo.title}
+                  width={80}
+                  height={140}
+                />
+              </div>
+              <div className="pl-1">
+                <p>title: {data.volumeInfo.title}</p>
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
